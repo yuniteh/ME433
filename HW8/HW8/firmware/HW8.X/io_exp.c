6acb3f66935620368ab34ee083af8a6e7958d167
yuniteh <yuniteh@MacBook-Pro.local>
2017-04-27 00:20:39 -0500

    created harmony project

---
+++ HW8/HW8/firmware/HW8.X/io_exp.c
@@ -0,0 +1,43 @@
+#include <xc.h>
+#include "io_exp.h"
+
+void init_exp() {
+    
+    i2c_master_setup();
+    
+    // initialize io direction
+    i2c_master_start();
+    i2c_master_send(ADD<<1|0);
+    i2c_master_send(0x00);
+    i2c_master_send(0xF0);
+    i2c_master_stop();
+ 
+    // set outputs to low
+    i2c_master_start();
+    i2c_master_send(ADD<<1|0);
+    i2c_master_send(0x09);
+    i2c_master_send(0x00);
+    i2c_master_stop();
+}
+
+void set_exp(char pin, char level) {
+    unsigned char write = level << pin;
+    i2c_master_start();
+    i2c_master_send(ADD<<1|0);
+    i2c_master_send(0x09);
+    i2c_master_send(write);
+    i2c_master_stop();
+}
+
+char get_exp() {
+    i2c_master_start();
+    i2c_master_send(ADD<<1|0);
+    i2c_master_send(0x09);
+    i2c_master_restart();
+    i2c_master_send(ADD<<1|1);
+    char out = i2c_master_recv();
+    i2c_master_ack(1);
+    i2c_master_stop();
+    
+    return out;
+}
